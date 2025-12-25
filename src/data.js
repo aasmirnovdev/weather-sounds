39e4f912830f642412545dca5021bc207eb95101
Smirnov Alex <aasmirnov@tsum.ru>
2025-12-25 11:55:39 +0300

    feat: добавляет пользовательскую функциональность

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import summerIcon from './assets/icons/sun.svg';
+import summerSound from './assets/sounds/summer.mp3';
+import summerBg from './assets/summer-bg.jpg';
+import rainIcon from './assets/icons/cloud-rain.svg';
+import rainSound from './assets/sounds/rain.mp3';
+import rainBg from './assets/rainy-bg.jpg';
+import winterIcon from './assets/icons/cloud-snow.svg';
+import winterSound from './assets/sounds/winter.mp3';
+import winterBg from './assets/winter-bg.jpg';
+
+export default [
+  {
+    name: 'summer',
+    icon: summerIcon,
+    sound: summerSound,
+    bg: summerBg,
+  },
+  {
+    name: 'rain',
+    icon: rainIcon,
+    sound: rainSound,
+    bg: rainBg,
+  },
+  {
+    name: 'winter',
+    icon: winterIcon,
+    sound: winterSound,
+    bg: winterBg,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,4 +1,65 @@
+import data from './data';
 import './main.scss';
 
-const a = document.querySelector('body');
-a.innerHTML = 'a';
+const DEFAULT_VOLUME = 0.5;
+const mainBlock = document.querySelector('.weather');
+const weatherList = document.querySelector('.sounds');
+const soundVolume = document.createElement('input');
+
+mainBlock.append(soundVolume);
+weatherList.classList.add('sound-list');
+soundVolume.type = 'range';
+soundVolume.min = 0;
+soundVolume.max = 100;
+soundVolume.value = DEFAULT_VOLUME * 100;
+
+function changeVolume(evt) {
+  const audio = document.querySelector('audio');
+  if (!audio) return;
+  audio.volume = evt.target.value / 100;
+}
+
+soundVolume.addEventListener('input', changeVolume);
+
+function playSound(src) {
+  let audio = document.querySelector('audio');
+
+  if (audio && audio.src.includes(src)) {
+    audio.paused ? audio.play() : audio.pause();
+    return;
+  }
+
+  if (audio) {
+    audio.pause();
+    audio.remove();
+  }
+
+  audio = document.createElement('audio');
+  audio.src = src;
+  audio.volume = soundVolume.value / 100;
+  weatherList.append(audio);
+  audio.play();
+}
+
+data.forEach(item => {
+  const button = document.createElement('button');
+  const icon = document.createElement('img');
+  const img = document.createElement('img');
+  const soundList = document.createElement('div');
+
+  icon.src = item.icon;
+  img.src = item.bg;
+  icon.classList.add('sound-icon');
+  img.classList.add('sound-bg');
+  button.classList.add('sound-button');
+
+  button.append(icon);
+  button.append(img);
+  weatherList.append(soundList);
+  soundList.append(button);
+
+  button.addEventListener('click', () => {
+    mainBlock.style.backgroundImage = `url(${item.bg})`;
+    playSound(item.sound);
+  });
+});
