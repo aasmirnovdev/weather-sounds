a1c8593ce6742d623c3946aaf5f212c9005ec9bc
Smirnov Alex <aasmirnov@tsum.ru>
2025-12-25 11:51:14 +0300

    feat: добавляет возможность обработки картинок и звуков для вебпака

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -39,6 +39,14 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(mp3)$/i,
+        type: 'asset/resource',
+      },
     ]
   }
 }
