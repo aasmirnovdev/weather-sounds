807c916bc631b57dbf101762b178dcbb815672a2
Smirnov Alex <aleksandr.smirnov@domrf.ru>
2026-01-22 19:32:35 +0300

    feat: тестовое задание для нормализации объекта

--- src/calc.ts
+++ src/calc.ts
@@ -18,3 +18,5 @@ const totalPrice = ({ price, discount = 0, isInstallment, months = 12 }: Props):
 
 const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
 console.log(price); // 6250
+
+export {totalPrice};
